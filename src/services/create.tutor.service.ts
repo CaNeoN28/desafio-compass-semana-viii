366dc5914f5a309de61c47683f40ad757a994448
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-28 15:36:49 -0400

    Validation completed for required fields

--- src/services/create.tutor.service.ts
+++ src/services/create.tutor.service.ts
@@ -5,7 +5,11 @@ class CreateUser {
   static handle = async function (data: ITutor) {
     const validationErrors = [];
     if (!data.date_of_birth) validationErrors.push("Date of birth is required");
-    if (!data.email) validationErrors.push("Email is required")
+    if (!data.email) validationErrors.push("Email is required");
+    if (!data.name) validationErrors.push("Name is required");
+    if (!data.password) validationErrors.push("Password is required");
+    if (!data.phone) validationErrors.push("Phone is required");
+    if (!data.zip_code) validationErrors.push("Zip code is required");
 
     if (validationErrors.length > 0) {
       const message = validationErrors.join(", ");
