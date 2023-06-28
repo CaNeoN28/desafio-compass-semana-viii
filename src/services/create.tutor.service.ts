67fe07898a2d4b5239cb8bb0a7a664185f2f73b1
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-28 16:20:23 -0400

    Validating name and password

--- src/services/create.tutor.service.ts
+++ src/services/create.tutor.service.ts
@@ -16,13 +16,39 @@ class CreateUser {
     }
 
     if (!data.email) validationErrors.push("Email is required");
+    else {
+      const email = data.email
+
+      if(!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/))
+        validationErrors.push("Please provide a valid email")
+
+      validatedData.email = email
+    }
+
     if (!data.name) validationErrors.push("Name is required");
+    else{
+      const name = data.name
+
+      if(name.length < 3)
+        validationErrors.push("Please provide a name with at least 3 characters")
+
+      validatedData.name = name
+    }
+
     if (!data.password) validationErrors.push("Password is required");
+    else{
+      const password = data.password
+
+      if(!password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d]{8,}$/))
+        validationErrors.push("The password should contain at least 8 characters, a number, an uppercase letter and a lowercase letter")
+
+      validatedData.password = password
+    }
     if (!data.phone) validationErrors.push("Phone is required");
     if (!data.zip_code) validationErrors.push("Zip code is required");
 
     if (validationErrors.length > 0) {
-      const message = validationErrors.join(", ");
+      const message = validationErrors.join("; ");
       throw new ValidationError(message, data);
     }
 
