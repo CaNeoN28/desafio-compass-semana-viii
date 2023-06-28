1ecbf9a42520d55c8b474ffde5c84db82b1833bd
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-28 16:01:54 -0400

    Validating date of birth

--- src/services/create.tutor.service.ts
+++ src/services/create.tutor.service.ts
@@ -1,10 +1,20 @@
+import UniqueFieldError from "../errors/UniqueFieldError";
 import ValidationError from "../errors/ValidationError";
+import TutorModel from "../models/Tutor.model";
 import ITutor from "../types/ITutor";
 
 class CreateUser {
   static handle = async function (data: ITutor) {
+    const validatedData: any = {};
     const validationErrors = [];
+
     if (!data.date_of_birth) validationErrors.push("Date of birth is required");
+    else {
+      validatedData.date_of_birth = new Date(data.date_of_birth);
+      if (!Date.parse(validatedData.date_of_birth))
+        validationErrors.push("Please provide a valid date");
+    }
+
     if (!data.email) validationErrors.push("Email is required");
     if (!data.name) validationErrors.push("Name is required");
     if (!data.password) validationErrors.push("Password is required");
@@ -15,6 +25,15 @@ class CreateUser {
       const message = validationErrors.join(", ");
       throw new ValidationError(message, data);
     }
+
+    const userAlreadyExists = await TutorModel.findOne({ email: data.email });
+
+    if (userAlreadyExists) {
+      const message = "Email already in use";
+      throw new UniqueFieldError(message, data);
+    }
+
+    return validatedData;
   };
 }
 
