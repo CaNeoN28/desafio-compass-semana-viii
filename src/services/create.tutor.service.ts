56baea90174130bbefc96347fa5e180cc6135feb
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-28 16:43:57 -0400

    Validating remaining userdata

--- src/services/create.tutor.service.ts
+++ src/services/create.tutor.service.ts
@@ -17,35 +17,61 @@ class CreateUser {
 
     if (!data.email) validationErrors.push("Email is required");
     else {
-      const email = data.email
+      const email = data.email;
 
-      if(!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/))
-        validationErrors.push("Please provide a valid email")
+      if (!email.match(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/))
+        validationErrors.push("Please provide a valid email");
 
-      validatedData.email = email
+      validatedData.email = email;
     }
 
     if (!data.name) validationErrors.push("Name is required");
-    else{
-      const name = data.name
+    else {
+      const name = data.name;
 
-      if(name.length < 3)
-        validationErrors.push("Please provide a name with at least 3 characters")
+      if (name.length < 3)
+        validationErrors.push(
+          "Please provide a name with at least 3 characters"
+        );
 
-      validatedData.name = name
+      validatedData.name = name;
     }
 
     if (!data.password) validationErrors.push("Password is required");
-    else{
-      const password = data.password
+    else {
+      const password = data.password;
 
-      if(!password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d]{8,}$/))
-        validationErrors.push("The password should contain at least 8 characters, a number, an uppercase letter and a lowercase letter")
+      if (!password.match(/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])[A-Za-z\d]{8,}$/))
+        validationErrors.push(
+          "The password should contain at least 8 characters, a number, an uppercase letter and a lowercase letter"
+        );
 
-      validatedData.password = password
+      validatedData.password = password;
     }
+
     if (!data.phone) validationErrors.push("Phone is required");
+    else {
+      const phone = data.phone;
+
+      if (
+        !phone.match(
+          /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/
+        )
+      )
+        validationErrors.push("Please provide a valid phone number");
+
+      validatedData.phone = phone;
+    }
+
     if (!data.zip_code) validationErrors.push("Zip code is required");
+    else {
+      const zip_code = data.zip_code;
+
+      if (!zip_code.match(/^[0-9]{5,9}$/))
+        validationErrors.push("Please provide a valid zipcode");
+
+      validatedData.zip_code = zip_code;
+    }
 
     if (validationErrors.length > 0) {
       const message = validationErrors.join("; ");
