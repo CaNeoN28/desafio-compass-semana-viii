4f8ca10aa9340c90b438077d1b0db7e3ada3be9c
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-29 16:34:13 -0400

    Conflict response in path

--- src/docs/paths/Tutors.ts
+++ src/docs/paths/Tutors.ts
@@ -1,4 +1,5 @@
 import { PathItem } from "swagger-jsdoc";
+import BaseTutor from "../components/schemas/Tutor/BaseTutor";
 
 const Tutors: PathItem = {
 	post: {
@@ -20,6 +21,23 @@ const Tutors: PathItem = {
 					"application/json": {
 						schema: {
 							$ref: "#/components/schemas/TutorResponse",
+						},
+					},
+				},
+			},
+			409: {
+				description: "Conflict",
+				content: {
+					"application/json": {
+						schema: {
+							type: "object",
+							properties: {
+								message: {
+									type: "string",
+									example: "Email already in use",
+								},
+								data: BaseTutor,
+							},
 						},
 					},
 				},
