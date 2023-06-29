ad0ad87b66e502bc4148966905979ec3f1d66d24
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-29 16:17:19 -0400

    Tutor schema

--- src/docs/components/schemas/Tutor.ts
+++ src/docs/components/schemas/Tutor.ts
@@ -1,10 +1,40 @@
-import {Schema} from "swagger-jsdoc";
+import { Schema } from "swagger-jsdoc";
 
 const Tutor: Schema = {
 	type: "object",
 	properties: {
+		name: {
+			type: "string",
+			required: true,
+		},
+		password: {
+			type: "string",
+			required: true,
+		},
+		phone: {
+			type: "string",
+			required: true,
+		},
+		email: {
+			type: "string",
+			required: true,
+			unique: true,
+		},
+		date_of_birth: {
+			type: "string",
+			required: true,
+		},
+		zip_code: {
+			type: "string",
+			required: true,
+		},
+		pets:{
+			type: "array",
+			items: {
+				$ref: "#/components/schemas/pet"
+			}
+		}
+	},
+};
 
-	}
-}
-
-export default Tutor
+export default Tutor;
