e78182fe9af42cf7670d6ebe9701dacf6ce827f6
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-29 16:36:52 -0400

    Adding bad request response

--- src/docs/paths/Tutors.ts
+++ src/docs/paths/Tutors.ts
@@ -25,6 +25,22 @@ const Tutors: PathItem = {
 					},
 				},
 			},
+			400: {
+				description: "Bad request",
+				content: {
+					"application/json": {
+						schema:{
+							type: "object",
+							properties: {
+								message: {
+									type: "string"
+								},
+								data: BaseTutor
+							}
+						}
+					}
+				}
+			},
 			409: {
 				description: "Conflict",
 				content: {
