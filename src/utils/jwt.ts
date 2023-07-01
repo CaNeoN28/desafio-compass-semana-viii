164d3d70bfd11b2126433cf2878df8966d0b6d89
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-30 21:31:25 -0400

    Catching errors in validateToken

--- src/utils/jwt.ts
+++ src/utils/jwt.ts
@@ -2,31 +2,39 @@ import mongoose from "mongoose";
 import ITutor from "../types/ITutor";
 import jwt from "jsonwebtoken";
 export default class JwtUtils {
-  static generateToken({
-    name,
-    email,
-    id,
-  }: {
-    name: string;
-    email: string;
-    id: mongoose.Types.ObjectId;
-  }) {
-    const token = jwt.sign(
-      {
-        name,
-        email,
-        id,
-      },
-      process.env.JWT_SECRET || "secret",
-      {
-        expiresIn: "1h"
-      }
-    );
+	static generateToken({
+		name,
+		email,
+		id,
+		role
+	}: {
+		name: string;
+		email: string;
+		id: mongoose.Types.ObjectId;
+		role: string
+	}) {
+		const token = jwt.sign(
+			{
+				name,
+				email,
+				id,
+				role
+			},
+			process.env.JWT_SECRET!,
+			{
+				expiresIn: "1h",
+			}
+		);
 
-    return token;
-  }
+		return token;
+	}
 
-  static validateToken(token: string) {
-    return jwt.verify(token, process.env.JWT_SECRET || "secret");
-  }
+	static validateToken(token: string) {
+		try {
+			const response = jwt.verify(token, process.env.JWT_SECRET!);
+			return response
+		} catch (err) {}
+
+		return
+	}
 }
