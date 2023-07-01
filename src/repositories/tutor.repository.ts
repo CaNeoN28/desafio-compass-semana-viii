ea97ff167a9481c21f08d6211a408f9faf371345
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-30 20:24:04 -0400

    Adding admin role to first user

--- src/repositories/tutor.repository.ts
+++ src/repositories/tutor.repository.ts
@@ -4,6 +4,11 @@ class UserRepository {
   static create = async function (data: any) {
     const tutor = new TutorModel(data)
 
+		const isFirstUser = !(await TutorModel.findOne())
+
+		if(isFirstUser)
+			tutor.role = 'admin'
+
     await tutor.save()
 
     const response = await TutorModel.findById(tutor.id).select({password: false})
