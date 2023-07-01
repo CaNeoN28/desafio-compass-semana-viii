4ed0ad6a5afaf58430b27bf6924dab3d11d9fcd3
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-06-30 21:31:43 -0400

    Setting up user in the req

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -1,17 +1,29 @@
 import { Request, RequestHandler } from "express";
 import mongoose from "mongoose";
+import JwtUtils from "../utils/jwt";
+import UnauthorizedError from "../errors/UnathourizedError";
 
 const authMiddleware: RequestHandler = (req, res, next) => {
-	const token = req.headers.authorization
+	const authorization: any = req.headers.authorization;
 
-	req.user = {
-		email: "email@example.com",
-		id: (new mongoose.Types.ObjectId()).toString(),
-		name: "example name",
-		role: "user"
-	}
+	if (!authorization || !authorization.startsWith("Bearer "))
+		throw new UnauthorizedError("Please provide a valid token");
 
-	return next()
-}
+	const token = authorization.split(" ")[1];
 
-export default authMiddleware
+	const user = JwtUtils.validateToken(token);
+
+	if(!user)
+		throw new UnauthorizedError("Please provide a valid token")
+
+	req.user = user as {
+		name: string;
+		email: string;
+		id: string;
+		role: string;
+	};
+
+	return next();
+};
+
+export default authMiddleware;
