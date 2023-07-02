b462d6f17bc2b62d2514cca0f8eacce75e7e5dc5
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-07-02 10:12:56 -0400

    Get documentation

--- src/docs/paths/Tutor/getTutors.ts
+++ src/docs/paths/Tutor/getTutors.ts
@@ -2,6 +2,37 @@ import { Operation } from "swagger-jsdoc";
 
 const getTutors: Operation = {
 	tags: ["Tutors"],
+	responses: {
+		200: {
+			description: "OK",
+			content: {
+				"application/json": {
+					schema: {
+						type: "array",
+						items: {
+							$ref: "#/components/schemas/TutorResponse",
+						},
+					},
+				},
+			},
+		},
+		401: {
+			$ref: "#/components/schemas/_401",
+		},
+		404: {
+			description: "Not found",
+			content: {
+				"application/json": {
+					schema: {
+						type: "object",
+						properties: {
+							message: { type: "string", example: "No tutors found!" },
+						},
+					},
+				},
+			},
+		},
+	},
 };
 
 export default getTutors;
