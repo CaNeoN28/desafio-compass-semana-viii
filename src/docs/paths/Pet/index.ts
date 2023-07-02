bfa400b3284a0e7fb3e9483611f24816d5d77d92
Carlos Felipe Steinheuser <carlosfelipe.steinheuser>
2023-07-02 15:03:57 -0400

    Finishing delete documentation

--- src/docs/paths/Pet/index.ts
+++ src/docs/paths/Pet/index.ts
@@ -1,13 +1,15 @@
 import { Paths } from "swagger-jsdoc";
 import postPet from "./postPet";
 import putPet from "./putPet";
+import deletePet from "./deletePet";
 
 const PetPaths: Paths = {
 	"/pet/{tutorId}": {
 		post: postPet
 	},
 	"/pet/{petId}/tutor/{tutorId}": {
-		put: putPet
+		put: putPet,
+		delete: deletePet
 	}
 }
 
